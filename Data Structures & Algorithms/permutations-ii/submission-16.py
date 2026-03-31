e5cb78e46d2d892fbe4e064cb59eaae27faf8a8e
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-03-31 10:41:46 +0800

    Add: permutations-ii - submission-16

---
+++ Data Structures & Algorithms/permutations-ii/submission-16.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        cnt = collections.Counter(nums)
+        res = []
+        sol = []
+        def dfs():
+            if len(sol) == len(nums):
+                res.append(sol[:])
+                return
+            
+
+            for n in cnt:
+                if cnt[n] > 0:
+                    sol.append(n)
+                    cnt[n] -= 1
+                    dfs()
+                    sol.pop()
+                    cnt[n] += 1
+        dfs()
+        return res
