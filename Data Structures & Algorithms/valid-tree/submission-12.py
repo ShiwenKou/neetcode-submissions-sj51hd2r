24cb50e05c124b0cdfef251c38d68f1ad6bf6888
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-02 15:23:53 +0800

    Add: valid-tree - submission-12

---
+++ Data Structures & Algorithms/valid-tree/submission-12.py
@@ -0,0 +1,29 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        par = [i for i in range(n)]
+        rank = [1] * n
+
+        def find(n1):
+            if n1 != par[n1]:
+                par[n1] = find(par[n1])
+            return par[n1]
+
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+            if p1 == p2:
+                return False
+            
+            if rank[p1] > rank[p2]: # attach p2 to p1
+                par[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                par[p1] = p2
+                rank[p2] += rank[p1]
+            return True
+        total = n
+        for n1, n2 in edges:
+            if union(n1, n2) == False: return False
+            total -= 1
+        return True if total == 1 else False
+
+    # tree 1 component and no loops
