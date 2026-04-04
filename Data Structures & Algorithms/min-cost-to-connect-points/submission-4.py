93de3d0d853af840b14d34e6e7273d80a4ff94a3
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-04 21:55:01 +0800

    Update: min-cost-to-connect-points - submission-4

--- Data Structures & Algorithms/min-cost-to-connect-points/submission-4.py
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-4.py
@@ -2,26 +2,27 @@ class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         adjList = collections.defaultdict(list)
         for i in range(len(points)):
-            x1, x2 = points[i]
             for j in range(len(points)):
-                if j == i:
-                    continue
-                y1, y2 = points[j]
-                dist = abs(x1 - y1) + abs(x2 - y2)
-                adjList[i].append([dist, j])
-                
-        seen = set()
-        minHeap = [[0,0]]
-        cost = 0
-        while len(seen) < len(points):
+                if i != j:
+                    x1, y1 = points[i]
+                    x2, y2 = points[j]
+                    dist = abs(x1 - x2) + abs(y1 - y2)
+                    adjList[i].append([dist, j])
         
+        seen = set()
+
+        minHeap = [[0, 0]] # dist, node
+        cost = 0
+        while minHeap:
+
             dist, node = heapq.heappop(minHeap)
             if node in seen:
                 continue
             seen.add(node)
             cost += dist
-
             for nei_dist, nei in adjList[node]:
-                if nei not in seen:
-                    heapq.heappush(minHeap, [nei_dist, nei])
+                if nei in seen:
+                    continue
+                
+                heapq.heappush(minHeap, [nei_dist, nei])
         return cost
