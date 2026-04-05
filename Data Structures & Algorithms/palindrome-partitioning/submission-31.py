5cea736a84912051437abf9aca5344df04c34e6e
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-05 14:01:58 +0800

    Add: palindrome-partitioning - submission-31

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-31.py
@@ -0,0 +1,21 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        
+
+        res = []
+        sol = []
+        def dfs(start):
+
+            if start == len(s):
+                res.append(sol[:])
+                return
+            for end in range(start, len(s)):
+                substring = s[start: end + 1]
+                if substring == substring[::-1]:
+                    sol.append(substring)
+                    dfs(end + 1)
+                    sol.pop()
+
+        dfs(0)
+        return res
+
