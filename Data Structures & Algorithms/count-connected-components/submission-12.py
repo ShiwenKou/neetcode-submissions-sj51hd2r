67d3350720e6747ede66323495ef61b6cddaa811
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-05 14:18:41 +0800

    Add: count-connected-components - submission-12

---
+++ Data Structures & Algorithms/count-connected-components/submission-12.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        
+        adjList = collections.defaultdict(list)
+
+        for n1, n2 in edges:
+            adjList[n1].append(n2)
+            adjList[n2].append(n1)
+        
+        # undirected graph, need to track the prev node
+        seen = set() 
+        def dfs(node, prev):
+
+            if node in seen:
+                return
+            seen.add(node)
+
+            for nei in adjList[node]:
+                if nei != prev:
+                    dfs(nei, node)
+
+        total = 0
+
+        for node in range(n):
+            if node not in seen:
+                total += 1
+                dfs(node, -1)
+        return total
