740a19b49e77a8260305605bb3692b50b870df8d
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-09 11:35:32 +0800

    Add: decode-ways - submission-18

---
+++ Data Structures & Algorithms/decode-ways/submission-18.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
+        from functools import cache
+        @cache
+        def dfs(start):
+
+            if start == len(s):
+                return 1
+            
+            res = 0
+            sub1 = s[start: start + 1]
+            if 1 <= int(sub1) <= 9:
+                res += dfs(start + 1)
+
+            if start + 1 < len(s):
+                sub2 = s[start: start + 2]
+
+                if sub2[0] != '0' and 10 <= int(sub2) <= 26:
+                    res += dfs(start + 2)
+            
+            return res
+
+        
+        return dfs(0)
