a7a0a83999b1ebe0249a227518ff9619c9b8e8a7
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-11 14:54:44 +0800

    Add: house-robber-ii - submission-8

---
+++ Data Structures & Algorithms/house-robber-ii/submission-8.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+
+        if len(nums) == 1:
+            return nums[0]
+        def helper(houses):
+            # if len(houses) == 1:
+            #     return houses[0]
+            memo = {}
+            def dfs(i):
+
+                if i >= len(houses):
+                    return 0
+                if i in memo:
+                    return memo[i]
+                res = max(dfs(i + 2) + houses[i], dfs(i + 1))
+                memo[i] = res
+                return res
+
+            return dfs(0)
+
+
+        return max(helper(nums[1:]), helper(nums[:-1]))
+            
