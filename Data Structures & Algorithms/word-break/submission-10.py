f35ec6732e9404b8fb156102f7a13109a39379eb
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-14 16:12:47 +0800

    Update: word-break - submission-10

--- Data Structures & Algorithms/word-break/submission-10.py
+++ Data Structures & Algorithms/word-break/submission-10.py
@@ -1,25 +1,21 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        
-
-
+        from functools import cache
         word_set = set(wordDict)
 
-        memo = {}
+        @cache
         def dfs(start):
-
             if start == len(s):
                 return True
-            if start in memo:
-                return memo[start]
+
+            
+
 
             for end in range(start, len(s)):
                 sub = s[start: end + 1]
                 if sub in word_set:
                     if dfs(end + 1):
-                        memo[start] = True
                         return True
-            memo[start] = False
             return False
         
         return dfs(0)
