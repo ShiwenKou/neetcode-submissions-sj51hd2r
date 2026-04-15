c82329ab6bb84e471ce69a192580e72654f505cd
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-15 23:03:42 +0800

    Add: pow-x-n - submission-7

---
+++ Data Structures & Algorithms/pow-x-n/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        from functools import cache
+        @cache
+        def dfs(n):
+            if n == 1:
+                return x
+            if n == -1:
+                return 1/x
+            if n == 0:
+                return 1
+
+            res = 1
+            if n % 2:
+                res = dfs(n // 2) * dfs(n // 2)  * x
+            else:
+                res = dfs(n // 2) * dfs(n // 2) 
+
+            return res
+        return dfs(n)
