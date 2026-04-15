777f0aa03c135ce910da3c0f82662a922544bf3a
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-15 12:23:43 +0800

    Add: valid-tree - submission-26

---
+++ Data Structures & Algorithms/valid-tree/submission-26.py
@@ -0,0 +1,33 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        par = [n for n in range(n)]
+        rank = [1] * n
+        # no loops, one components
+
+        def find(n1):
+            if n1 != par[n1]:
+                par[n1] = find(par[n1]) # flattent the tree
+            return par[n1]
+        
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2: # 两个节点par是一起
+                return False
+
+            if rank[p1] > rank[p2]:
+                par[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                par[p1] = p2
+                rank[p2] += rank[p1]
+
+            return True
+
+        component = n
+        for n1, n2 in edges:
+            if not union(n1, n2):
+                return False
+            component -= 1
+        return component == 1
+            
