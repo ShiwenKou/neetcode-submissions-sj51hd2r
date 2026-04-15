5158d0dfaf72476b9e50247b66a865b908a3e7f4
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-15 11:13:20 +0800

    Update: eating-bananas - submission-22

--- Data Structures & Algorithms/eating-bananas/submission-22.py
+++ Data Structures & Algorithms/eating-bananas/submission-22.py
@@ -1,26 +1,26 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         
+        # left = 1, right = max(piles)
+
+        left = 1
+        right = max(piles)
+        res = 0
         def calc_time(speed, piles):
             time = 0
             for p in piles:
                 time += math.ceil(p / speed)
             return time
-
-
-        left, right = 1, max(piles)
-        res = max(piles)
         while left <= right:
 
             mid = (left + right) // 2
 
             time = calc_time(mid, piles)
 
-            if time <= h: # eat ok. but can you eat slower?
-                res = min(mid, res)
-
+            if time <= h:
+                res = mid
                 right = mid - 1
-
             else:
                 left = mid + 1
+        
         return res
