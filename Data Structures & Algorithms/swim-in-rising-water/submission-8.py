709ad4a209f444eb8e0bfb337bd92a4253aca842
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-17 09:24:22 +0800

    Add: swim-in-rising-water - submission-8

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-8.py
@@ -0,0 +1,25 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        
+        minHeap = [(grid[0][0], 0, 0)]
+
+        seen = set()
+
+        while minHeap:
+
+            cost, r, c = heapq.heappop(minHeap) # O(logn)
+            
+            if (r, c) in seen:
+                continue
+
+            seen.add((r, c))
+            if r == c == len(grid) - 1:
+                return cost
+            for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                nr = dr + r
+                nc = dc + c
+
+                if (nr not in range(len(grid)) or nc not in range(len(grid[0])) or
+                    (nr, nc) in seen):
+                    continue
+                heapq.heappush(minHeap, (max(grid[nr][nc], cost), nr, nc))
