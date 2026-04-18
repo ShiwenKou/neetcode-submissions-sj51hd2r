5823c0c50bf8fa47e43bdcfa2aa7e9b635e79fdc
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-18 16:13:45 +0800

    Add: longest-increasing-subsequence - submission-33

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-33.py
@@ -0,0 +1,26 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        
+        tails = []
+
+        def findLeft(tails, n):
+            left, right = 0, len(tails) - 1
+            res = len(tails)
+            while left <= right:
+                mid = (left + right) // 2
+
+                if tails[mid] >= n:
+                    res = mid
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            return res
+
+        for n in nums:
+            idx = findLeft(tails, n)
+
+            if idx == len(tails):
+                tails.append(n)
+            else:
+                tails[idx] = n
+        return len(tails)
