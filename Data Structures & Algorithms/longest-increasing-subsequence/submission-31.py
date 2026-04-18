8d4ba1fe0c10a87c1deec68d09c8fd8acf8ebe56
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-18 16:07:02 +0800

    Add: longest-increasing-subsequence - submission-31

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-31.py
@@ -0,0 +1,21 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        sys.setrecursionlimit(9999)
+        from functools import cache
+
+        @cache
+        def dfs(i, prev):
+
+            if i == len(nums):
+                return 0
+
+            
+            if nums[i] > prev:
+                # take or not
+                res = max(dfs(i + 1, nums[i]) + 1, dfs(i + 1, prev))
+            else:
+                res = dfs(i + 1, prev)
+            
+            return res
+        
+        return dfs(0, float('-inf'))
