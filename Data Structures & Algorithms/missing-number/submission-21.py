f1a3e94ae20627237de3124bf34564d418e90c49
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-21 13:23:47 +0800

    Add: missing-number - submission-21

---
+++ Data Structures & Algorithms/missing-number/submission-21.py
@@ -0,0 +1,22 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+
+        for i in range(len(nums)):
+            nums[i] = nums[i] + 1
+        #不能标记n + 1, 但是我们可以标记 1 到n 。
+        
+        for i in range(len(nums)):
+
+            val = abs(nums[i])
+
+            if val <= len(nums):
+                idx = val - 1
+                nums[idx] = -abs(nums[idx])
+        
+        for i in range(len(nums)):
+
+            if nums[i] > 0:
+                return i
+        
+        return len(nums)
