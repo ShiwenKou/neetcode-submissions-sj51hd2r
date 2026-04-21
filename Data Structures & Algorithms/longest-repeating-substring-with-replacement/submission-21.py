64993c6cba63ca77d7a8aa1df651dedd51dd73c6
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-21 12:01:00 +0800

    Add: longest-repeating-substring-with-replacement - submission-21

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-21.py
@@ -0,0 +1,16 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        window = collections.Counter()
+
+        left = 0
+        res = 0
+        most_common = 0
+        for right in range(len(s)):
+            window[s[right]] += 1
+            most_common = max(most_common, window[s[right]])
+            while right - left + 1 - most_common  > k:
+                window[s[left]] -= 1
+                left += 1
+            
+            res = max(res, right - left + 1)
+        return res
