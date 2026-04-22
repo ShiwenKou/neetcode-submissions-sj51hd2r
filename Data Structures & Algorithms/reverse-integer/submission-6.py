39db7ceabba0f1d9b8a22b7b899cfc86aefcc646
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-22 10:29:56 +0800

    Add: reverse-integer - submission-6

---
+++ Data Structures & Algorithms/reverse-integer/submission-6.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        max_int = 0x7FFFFFFF
+        if x >= 0:
+            sign = 1
+        else:
+            sign = -1
+        
+
+        x = abs(x)
+        res = 0
+        while x:
+            val = x % 10
+            res = res* 10 + val
+            x = x // 10
+        
+        if res > max_int:
+            return 0
+        
+        else:
+            return res * sign
