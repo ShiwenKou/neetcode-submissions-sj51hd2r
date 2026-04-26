4dab20c6f4919698ebb5c2640de672968818b084
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-26 09:33:45 +0800

    Add: counting-bits - submission-13

---
+++ Data Structures & Algorithms/counting-bits/submission-13.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        
+        memo = [0] * (n + 1)
+        offset = 1
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            
+            memo[i] = 1 + memo[i - offset]
+        return memo
