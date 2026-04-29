65fa313f8064ba2cb37516566d3e08a4515953a4
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-29 11:48:45 +0800

    Add: word-break - submission-14

---
+++ Data Structures & Algorithms/word-break/submission-14.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        from functools import cache
+        wordSet = set(wordDict)
+
+        @cache
+        def dfs(start):
+            if start == len(s):
+                return True
+
+
+            for end in range(start, len(s)): # because len(s) is exclusive. and you will neven slice the last char
+                sub = s[start: end + 1]
+                if sub in wordSet:
+                    if dfs(end + 1):
+                        return True
+            return False
+
+        return dfs(0)
