e7df8d35f28a48337d4481e83329b83ba7f944af
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-29 09:28:33 +0800

    Add: pow-x-n - submission-22

---
+++ Data Structures & Algorithms/pow-x-n/submission-22.py
@@ -0,0 +1,24 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        def dfs(n):
+            if n == 1:
+                return x
+
+            if n == 0:
+                return 1
+
+            half = dfs(n // 2)
+
+            if n % 2:
+                res = half * half * x
+
+            else:
+                res = half * half
+
+            return res
+        
+        if n < 0:
+            x = 1/x
+            n = abs(n)
+        return dfs(n)
