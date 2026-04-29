47417c4e07adbf3a001e267c048902032105eec6
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-29 12:23:08 +0800

    Add: non-cyclical-number - submission-9

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-9.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+        def helper(n):
+
+            return sum(int(c) ** 2 for c in str(n))
+
+        
+        slow = n
+        fast = helper(n)
+
+
+        while fast != 1 and fast != slow:
+            fast = helper(helper(fast))
+            slow = helper(slow)
+
+        return fast == 1
