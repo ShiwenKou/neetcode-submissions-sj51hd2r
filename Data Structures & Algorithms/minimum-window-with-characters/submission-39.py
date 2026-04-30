a94eeb6a60f5f6531d5e536122ebf34e01bcf96b
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-04-30 14:06:30 +0800

    Add: minimum-window-with-characters - submission-39

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-39.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        pattern = collections.Counter(t)
+        window = collections.Counter()
+        have = 0
+        need = len(pattern)
+
+        left = 0
+        res = ''
+        length = float('inf')
+        for right in range(len(s)):
+            if s[right] in pattern:
+
+                window[s[right]] += 1
+                if window[s[right]] == pattern[s[right]]:
+                    have += 1
+            while have == need:
+
+                if right - left + 1 < length:
+                    res = s[left:right + 1]
+                    length = right - left + 1
+                
+                if s[left] in pattern:
+                    window[s[left]] -= 1
+                    if window[s[left]] < pattern[s[left]]:
+                        have -= 1
+                left += 1
+        return res
+
