c0f81fc7beca8c855228331f2cd3a4375ecd99a7
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-05-01 10:19:42 +0800

    Add: largest-number - submission-39

---
+++ Data Structures & Algorithms/largest-number/submission-39.py
@@ -0,0 +1,9 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        
+        from functools import cmp_to_key
+
+
+        res = sorted(map(str, nums), key=cmp_to_key(lambda x1, x2 :1 if x1 + x2 < x2 + x1 else -1))
+
+        return ''.join(res) if res[0] != '0' else '0'
