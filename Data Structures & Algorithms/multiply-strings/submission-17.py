4e2474fbe8f509c93c9b4c98f7e434567622a779
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-05-09 13:44:25 +0800

    Add: multiply-strings - submission-17

---
+++ Data Structures & Algorithms/multiply-strings/submission-17.py
@@ -0,0 +1,19 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        
+        res = [0] * (len(num1) + len(num2))
+        for i in range(len(num1) - 1, -1, -1):
+            for j in range(len(num2) -1, -1, -1):
+
+                mul = int(num1[i]) * int(num2[j])
+
+                p1, p2 = i + j, i + j + 1
+
+                total = res[p2] + mul
+
+                res[p2] = total % 10
+                res[p1] = res[p1] + total // 10
+
+        result = ''.join(map(str, res)).lstrip('0')
+
+        return result or '0'
