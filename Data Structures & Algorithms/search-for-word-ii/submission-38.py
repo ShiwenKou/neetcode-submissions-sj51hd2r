cb7d8686a4dd0c8fe6b0a93ce7ce72d151dd3da6
ShiwenKou <50741047+ShiwenKou@users.noreply.github.com>
2026-05-10 13:14:08 +0800

    Add: search-for-word-ii - submission-38

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-38.py
@@ -0,0 +1,51 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+    def add(self, word):
+        cur = self
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.end = True
+                
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        
+        curr = TrieNode()
+
+        for word in words:
+            curr.add(word)
+
+        sol = []
+        seen = set()
+        res = set()
+        def dfs(r, c, curr):
+
+            if (r not in range(len(board)) or c not in range(len(board[0])) or 
+                board[r][c] not in curr.children or (r, c) in seen):
+                return
+            
+
+            sol.append(board[r][c])
+            seen.add((r, c))
+            curr = curr.children[board[r][c]]
+            if curr.end:
+                res.add(''.join(sol))
+
+            dfs(r - 1, c, curr)
+            dfs(r + 1, c, curr)
+            dfs(r, c + 1, curr)
+            dfs(r, c - 1, curr)
+            
+            seen.remove((r, c))
+            sol.pop()
+
+
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                dfs(r, c, curr)
+
+        return list(res)
